12f2afb71ebc1fbb3f0f60c48d0fd1e0dccc2b32
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2021-12-23 22:02:54 +0500

    init file

---
+++ src/main/java/com/example/finalproject/Model/User.java
@@ -0,0 +1,19 @@
+package com.example.finalproject.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+    private List<Role> roles;
+}
